20b35ba64c14ac05287699dd11b7cc8276618dc8
S0ph1ia <badalovasophia@gmail.com>
2020-11-30 17:10:11 +0000

    Initial commit for 4000! code

---
+++ src/main/java/com/benchmark/BenchMark.java
@@ -0,0 +1,67 @@
+package com.benchmark;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.BufferedWriter;
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@BenchmarkMode(Mode.All)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class BenchMark implements HttpFunction {
+
+
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+        BufferedWriter writer = httpResponse.getWriter();
+
+        main();
+
+        writer.write("Implemented the Benchmark CPU- factorial 4000 ");
+    }
+
+    @State(Scope.Thread)
+    public static class MyValues {
+        public static final Logger logger = Logger.getLogger(BenchMark.class.getName());
+    }
+
+    @Benchmark
+    @Fork(0)
+    public void factorial_4000(Blackhole bh) {
+        BigInteger result = factorial(new BigInteger("4000"));
+        bh.consume(result);
+    }
+
+    public void main() throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(BenchMark.class.getSimpleName())
+                .warmupIterations(20)
+                .measurementIterations(20)
+                .build();
+        Collection<RunResult> runResults = new Runner(opt).run();
+        MyValues.logger.log(Level.INFO, " The Factorial benchmark has run ");
+    }
+
+    private BigInteger factorial(BigInteger num) {
+        BigInteger total;
+        if (num.equals(new BigInteger("1"))) {
+            return new BigInteger("1");
+        } else {
+            total = num.multiply(factorial(num.subtract(new BigInteger("1"))));
+        }
+        return total;
+    }
+}
+
